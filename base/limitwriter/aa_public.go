e53713f21e1b56a2ad5d7eb7f411bf5d46803a20
Twigg <twigg@twigg.vc>
2026-09-05 22:53:59 +0000

    Return a struct from the limitwriter constructor
    Twigg mirror c/1706v1

--- base/limitwriter/aa_public.go
+++ base/limitwriter/aa_public.go
@@ -9,10 +9,14 @@ var ErrNotEnoughQuota = errors.New("not enough quota")
 
 // Writer that writes to a destination but stops with
 // ErrNotEnoughQuota after n bytes.
-type LimitWriter interface {
-	io.Writer
+type LimitWriter struct {
+	w *limitWriter
+}
+
+func (l LimitWriter) Write(p []byte) (int, error) {
+	return l.w.Write(p)
 }
 
 func New(w io.Writer, n int64) LimitWriter {
-	return newLimitWriter(w, n)
+	return LimitWriter{newLimitWriter(w, n)}
 }
